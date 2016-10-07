a41a38534e3b95823d10ac976fbe2f2ca787d598
Vanja Veselinovic <vanja.veselinovic.95@gmail.com>
2016-10-07 16:09:56 -0400

    Road Line Painter game

---
+++ app/src/main/java/com/vanjav/roadlinepainter/RoadLinePainterView.java
@@ -0,0 +1,84 @@
+package com.vanjav.roadlinepainter;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Rect;
+import android.graphics.Region;
+import android.support.v4.content.ContextCompat;
+import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.view.Choreographer;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+/**
+ * Created by vveselin on 07/10/2016.
+ */
+
+public class RoadLinePainterView extends SurfaceView  implements Choreographer.FrameCallback{
+    private int width, height;
+    private SurfaceHolder surfaceHolder;
+    private long previousFrameNanos;
+    private Canvas canvas;
+    private Paint paint;
+
+    public RoadLinePainterView(Context context) {
+        this(context, null);
+    }
+
+    public RoadLinePainterView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        ((Activity) getContext()).getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        width = displayMetrics.widthPixels;
+        height = displayMetrics.heightPixels;
+
+        paint = new Paint();
+        paint.setColor(ContextCompat.getColor(getContext(), R.color.colorAccent));
+
+        surfaceHolder = getHolder();
+
+        previousFrameNanos = System.nanoTime();
+
+        Choreographer.getInstance().postFrameCallback(this);
+    }
+
+    @Override
+    public void doFrame(long frameTimeNanos) {
+        Choreographer.getInstance().postFrameCallback(this);
+
+        update((frameTimeNanos - previousFrameNanos)/1000000);
+        draw();
+
+        previousFrameNanos = frameTimeNanos;
+    }
+
+    private void update(long deltaTimeNanos) {
+
+    }
+
+    private void draw() {
+        try {
+            canvas = surfaceHolder.lockCanvas(null);
+            if(canvas != null){
+                synchronized (surfaceHolder) {
+                    canvas.drawRect(0, 0, width, height, paint);
+                }
+            }
+        }
+        finally {
+            if (canvas != null) {
+                surfaceHolder.unlockCanvasAndPost(canvas);
+            }
+        }
+    }
+
+    public boolean onTouchEvent(MotionEvent event) {
+        return true;
+    }
+}
