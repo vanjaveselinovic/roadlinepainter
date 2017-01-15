1ffa5d97f04b530c0870caf0a812849fc942f47c
Vanja Veselinovic <vanja.veselinovic.95@gmail.com>
2017-01-14 19:09:18 -0500

    restart activity on button click

--- app/src/main/java/com/vanjav/roadlinepainter/MainActivity.java
+++ app/src/main/java/com/vanjav/roadlinepainter/MainActivity.java
@@ -2,15 +2,24 @@ package com.vanjav.roadlinepainter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     RoadLinePainterView roadLinePainterView;
+    Button restartButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         roadLinePainterView = (RoadLinePainterView) findViewById(R.id.road_line_painter_view);
+        restartButton = (Button) findViewById(R.id.game_over_button);
+        restartButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick (View v) {
+                MainActivity.super.recreate();
+            }
+        });
     }
 
     @Override
